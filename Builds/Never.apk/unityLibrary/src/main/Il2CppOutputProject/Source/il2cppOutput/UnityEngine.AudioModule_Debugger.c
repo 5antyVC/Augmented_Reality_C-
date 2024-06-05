6ab4879c88480fb29c24d9528da5299efb1ced9a
SantyVC <santiagovelasquezcalderon.com>
2024-06-05 11:34:42 +0200

    App Unity Realidad Aumentada Flechas

---
+++ Builds/Never.apk/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/UnityEngine.AudioModule_Debugger.c
@@ -0,0 +1,298 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodExecutionContextInfo g_methodExecutionContextInfos[1] = { { 0, 0, 0 } };
+#else
+static const Il2CppMethodExecutionContextInfo g_methodExecutionContextInfos[1] = { { 0, 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const char* g_methodExecutionContextInfoStrings[1] = { NULL };
+#else
+static const char* g_methodExecutionContextInfoStrings[1] = { NULL };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodExecutionContextInfoIndex g_methodExecutionContextInfoIndexes[28] = 
+{
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+};
+#else
+static const Il2CppMethodExecutionContextInfoIndex g_methodExecutionContextInfoIndexes[1] = { { 0, 0} };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+IL2CPP_EXTERN_C Il2CppSequencePoint g_sequencePointsUnityEngine_AudioModule[];
+Il2CppSequencePoint g_sequencePointsUnityEngine_AudioModule[131] = 
+{
+	{ 37398, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 0 },
+	{ 37398, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 1 },
+	{ 37398, 1, 357, 357, 9, 10, 0, kSequencePointKind_Normal, 0, 2 },
+	{ 37398, 1, 358, 358, 13, 53, 1, kSequencePointKind_Normal, 0, 3 },
+	{ 37398, 1, 358, 358, 0, 0, 10, kSequencePointKind_Normal, 0, 4 },
+	{ 37398, 1, 359, 359, 17, 63, 13, kSequencePointKind_Normal, 0, 5 },
+	{ 37398, 1, 359, 359, 17, 63, 19, kSequencePointKind_StepOut, 0, 6 },
+	{ 37398, 1, 360, 360, 9, 10, 25, kSequencePointKind_Normal, 0, 7 },
+	{ 37399, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 8 },
+	{ 37399, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 9 },
+	{ 37399, 1, 364, 364, 16, 51, 0, kSequencePointKind_Normal, 0, 10 },
+	{ 37399, 1, 364, 364, 16, 51, 11, kSequencePointKind_StepOut, 0, 11 },
+	{ 37400, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 12 },
+	{ 37400, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 13 },
+	{ 37400, 1, 368, 368, 16, 48, 0, kSequencePointKind_Normal, 0, 14 },
+	{ 37400, 1, 368, 368, 16, 48, 11, kSequencePointKind_StepOut, 0, 15 },
+	{ 37405, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 16 },
+	{ 37405, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 17 },
+	{ 37405, 1, 393, 393, 17, 21, 0, kSequencePointKind_Normal, 0, 18 },
+	{ 37406, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 19 },
+	{ 37406, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 20 },
+	{ 37406, 1, 394, 394, 17, 29, 0, kSequencePointKind_Normal, 0, 21 },
+	{ 37407, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 22 },
+	{ 37407, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 23 },
+	{ 37407, 1, 401, 401, 17, 18, 0, kSequencePointKind_Normal, 0, 24 },
+	{ 37407, 1, 402, 402, 21, 51, 1, kSequencePointKind_Normal, 0, 25 },
+	{ 37407, 1, 403, 403, 17, 18, 9, kSequencePointKind_Normal, 0, 26 },
+	{ 37408, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 27 },
+	{ 37408, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 28 },
+	{ 37408, 1, 426, 426, 13, 14, 0, kSequencePointKind_Normal, 0, 29 },
+	{ 37408, 1, 427, 427, 17, 39, 1, kSequencePointKind_Normal, 0, 30 },
+	{ 37408, 1, 427, 427, 17, 39, 2, kSequencePointKind_StepOut, 0, 31 },
+	{ 37408, 1, 427, 427, 0, 0, 13, kSequencePointKind_Normal, 0, 32 },
+	{ 37408, 1, 428, 428, 17, 18, 16, kSequencePointKind_Normal, 0, 33 },
+	{ 37408, 1, 429, 429, 21, 38, 17, kSequencePointKind_Normal, 0, 34 },
+	{ 37408, 1, 429, 429, 21, 38, 18, kSequencePointKind_StepOut, 0, 35 },
+	{ 37408, 1, 430, 430, 21, 47, 24, kSequencePointKind_Normal, 0, 36 },
+	{ 37408, 1, 430, 430, 21, 47, 24, kSequencePointKind_StepOut, 0, 37 },
+	{ 37408, 1, 430, 430, 0, 0, 30, kSequencePointKind_Normal, 0, 38 },
+	{ 37408, 1, 431, 431, 21, 22, 33, kSequencePointKind_Normal, 0, 39 },
+	{ 37408, 1, 432, 432, 25, 39, 34, kSequencePointKind_Normal, 0, 40 },
+	{ 37408, 1, 432, 432, 25, 39, 34, kSequencePointKind_StepOut, 0, 41 },
+	{ 37408, 1, 432, 432, 0, 0, 40, kSequencePointKind_Normal, 0, 42 },
+	{ 37408, 1, 433, 433, 29, 47, 43, kSequencePointKind_Normal, 0, 43 },
+	{ 37408, 1, 433, 433, 29, 47, 43, kSequencePointKind_StepOut, 0, 44 },
+	{ 37408, 1, 433, 433, 0, 0, 49, kSequencePointKind_Normal, 0, 45 },
+	{ 37408, 1, 435, 435, 29, 48, 51, kSequencePointKind_Normal, 0, 46 },
+	{ 37408, 1, 435, 435, 29, 48, 51, kSequencePointKind_StepOut, 0, 47 },
+	{ 37408, 1, 436, 436, 21, 22, 57, kSequencePointKind_Normal, 0, 48 },
+	{ 37408, 1, 437, 437, 21, 52, 58, kSequencePointKind_Normal, 0, 49 },
+	{ 37408, 1, 437, 437, 0, 0, 67, kSequencePointKind_Normal, 0, 50 },
+	{ 37408, 1, 438, 438, 25, 50, 70, kSequencePointKind_Normal, 0, 51 },
+	{ 37408, 1, 438, 438, 25, 50, 76, kSequencePointKind_StepOut, 0, 52 },
+	{ 37408, 1, 439, 439, 17, 18, 82, kSequencePointKind_Normal, 0, 53 },
+	{ 37408, 1, 440, 440, 13, 14, 83, kSequencePointKind_Normal, 0, 54 },
+	{ 37409, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 55 },
+	{ 37409, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 56 },
+	{ 37409, 1, 444, 444, 13, 14, 0, kSequencePointKind_Normal, 0, 57 },
+	{ 37409, 1, 445, 445, 17, 46, 1, kSequencePointKind_Normal, 0, 58 },
+	{ 37409, 1, 445, 445, 17, 46, 1, kSequencePointKind_StepOut, 0, 59 },
+	{ 37409, 1, 446, 446, 13, 14, 9, kSequencePointKind_Normal, 0, 60 },
+	{ 37410, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 61 },
+	{ 37410, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 62 },
+	{ 37410, 1, 449, 449, 13, 14, 0, kSequencePointKind_Normal, 0, 63 },
+	{ 37410, 1, 450, 450, 17, 50, 1, kSequencePointKind_Normal, 0, 64 },
+	{ 37410, 1, 450, 450, 17, 50, 1, kSequencePointKind_StepOut, 0, 65 },
+	{ 37410, 1, 451, 451, 13, 14, 7, kSequencePointKind_Normal, 0, 66 },
+	{ 37411, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 67 },
+	{ 37411, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 68 },
+	{ 37411, 1, 454, 454, 13, 14, 0, kSequencePointKind_Normal, 0, 69 },
+	{ 37411, 1, 455, 455, 17, 49, 1, kSequencePointKind_Normal, 0, 70 },
+	{ 37411, 1, 455, 455, 17, 49, 1, kSequencePointKind_StepOut, 0, 71 },
+	{ 37411, 1, 456, 456, 13, 14, 7, kSequencePointKind_Normal, 0, 72 },
+	{ 37412, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 73 },
+	{ 37412, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 74 },
+	{ 37412, 1, 646, 646, 9, 10, 0, kSequencePointKind_Normal, 0, 75 },
+	{ 37412, 1, 647, 647, 13, 45, 1, kSequencePointKind_Normal, 0, 76 },
+	{ 37412, 1, 647, 647, 0, 0, 11, kSequencePointKind_Normal, 0, 77 },
+	{ 37412, 1, 648, 648, 17, 43, 14, kSequencePointKind_Normal, 0, 78 },
+	{ 37412, 1, 648, 648, 17, 43, 21, kSequencePointKind_StepOut, 0, 79 },
+	{ 37412, 1, 649, 649, 9, 10, 27, kSequencePointKind_Normal, 0, 80 },
+	{ 37413, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 81 },
+	{ 37413, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 82 },
+	{ 37413, 1, 653, 653, 9, 10, 0, kSequencePointKind_Normal, 0, 83 },
+	{ 37413, 1, 654, 654, 13, 50, 1, kSequencePointKind_Normal, 0, 84 },
+	{ 37413, 1, 654, 654, 0, 0, 11, kSequencePointKind_Normal, 0, 85 },
+	{ 37413, 1, 655, 655, 17, 52, 14, kSequencePointKind_Normal, 0, 86 },
+	{ 37413, 1, 655, 655, 17, 52, 21, kSequencePointKind_StepOut, 0, 87 },
+	{ 37413, 1, 656, 656, 9, 10, 27, kSequencePointKind_Normal, 0, 88 },
+	{ 37418, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 89 },
+	{ 37418, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 90 },
+	{ 37418, 2, 177, 177, 9, 10, 0, kSequencePointKind_Normal, 0, 91 },
+	{ 37418, 2, 178, 178, 13, 47, 1, kSequencePointKind_Normal, 0, 92 },
+	{ 37418, 2, 178, 178, 0, 0, 11, kSequencePointKind_Normal, 0, 93 },
+	{ 37418, 2, 180, 180, 17, 69, 14, kSequencePointKind_Normal, 0, 94 },
+	{ 37418, 2, 180, 180, 17, 69, 22, kSequencePointKind_StepOut, 0, 95 },
+	{ 37418, 2, 181, 181, 9, 10, 28, kSequencePointKind_Normal, 0, 96 },
+	{ 37419, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 97 },
+	{ 37419, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 98 },
+	{ 37419, 2, 185, 185, 9, 10, 0, kSequencePointKind_Normal, 0, 99 },
+	{ 37419, 2, 186, 186, 13, 46, 1, kSequencePointKind_Normal, 0, 100 },
+	{ 37419, 2, 186, 186, 0, 0, 11, kSequencePointKind_Normal, 0, 101 },
+	{ 37419, 2, 187, 187, 17, 75, 14, kSequencePointKind_Normal, 0, 102 },
+	{ 37419, 2, 187, 187, 17, 75, 22, kSequencePointKind_StepOut, 0, 103 },
+	{ 37419, 2, 188, 188, 9, 10, 28, kSequencePointKind_Normal, 0, 104 },
+	{ 37422, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 105 },
+	{ 37422, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 106 },
+	{ 37422, 3, 49, 49, 9, 10, 0, kSequencePointKind_Normal, 0, 107 },
+	{ 37422, 3, 50, 50, 13, 29, 1, kSequencePointKind_Normal, 0, 108 },
+	{ 37422, 3, 51, 51, 9, 10, 10, kSequencePointKind_Normal, 0, 109 },
+	{ 37423, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 110 },
+	{ 37423, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 111 },
+	{ 37423, 3, 64, 64, 9, 10, 0, kSequencePointKind_Normal, 0, 112 },
+	{ 37423, 3, 65, 65, 13, 53, 1, kSequencePointKind_Normal, 0, 113 },
+	{ 37423, 3, 65, 65, 13, 53, 2, kSequencePointKind_StepOut, 0, 114 },
+	{ 37423, 3, 65, 65, 13, 53, 9, kSequencePointKind_StepOut, 0, 115 },
+	{ 37423, 3, 65, 65, 13, 53, 14, kSequencePointKind_StepOut, 0, 116 },
+	{ 37423, 3, 66, 66, 9, 10, 22, kSequencePointKind_Normal, 0, 117 },
+	{ 37424, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 118 },
+	{ 37424, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 119 },
+	{ 37424, 4, 47, 47, 9, 10, 0, kSequencePointKind_Normal, 0, 120 },
+	{ 37424, 4, 48, 48, 13, 29, 1, kSequencePointKind_Normal, 0, 121 },
+	{ 37424, 4, 49, 49, 9, 10, 10, kSequencePointKind_Normal, 0, 122 },
+	{ 37425, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 123 },
+	{ 37425, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 124 },
+	{ 37425, 4, 62, 62, 9, 10, 0, kSequencePointKind_Normal, 0, 125 },
+	{ 37425, 4, 63, 63, 13, 53, 1, kSequencePointKind_Normal, 0, 126 },
+	{ 37425, 4, 63, 63, 13, 53, 2, kSequencePointKind_StepOut, 0, 127 },
+	{ 37425, 4, 63, 63, 13, 53, 9, kSequencePointKind_StepOut, 0, 128 },
+	{ 37425, 4, 63, 63, 13, 53, 14, kSequencePointKind_StepOut, 0, 129 },
+	{ 37425, 4, 64, 64, 9, 10, 22, kSequencePointKind_Normal, 0, 130 },
+};
+#else
+extern Il2CppSequencePoint g_sequencePointsUnityEngine_AudioModule[];
+Il2CppSequencePoint g_sequencePointsUnityEngine_AudioModule[1] = { { 0, 0, 0, 0, 0, 0, 0, kSequencePointKind_Normal, 0, 0, } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppCatchPoint g_catchPoints[1] = { { 0, 0, 0, 0, } };
+#else
+static const Il2CppCatchPoint g_catchPoints[1] = { { 0, 0, 0, 0, } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppSequencePointSourceFile g_sequencePointSourceFiles[] = {
+{ "", { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} },
+{ "\\home\\bokken\\build\\output\\unity\\unity\\Modules\\Audio\\Public\\ScriptBindings\\Audio.bindings.cs", { 161, 185, 143, 17, 109, 79, 89, 248, 70, 236, 39, 209, 120, 215, 116, 156} },
+{ "\\home\\bokken\\build\\output\\unity\\unity\\Modules\\Audio\\Public\\ScriptBindings\\AudioSampleProvider.bindings.cs", { 47, 120, 50, 45, 60, 26, 245, 52, 137, 63, 13, 94, 178, 230, 94, 160} },
+{ "\\home\\bokken\\build\\output\\unity\\unity\\Modules\\Audio\\Public\\ScriptBindings\\AudioClipPlayable.bindings.cs", { 117, 58, 219, 81, 174, 32, 141, 165, 250, 138, 140, 83, 41, 27, 0, 60} },
+{ "\\home\\bokken\\build\\output\\unity\\unity\\Modules\\Audio\\Public\\ScriptBindings\\AudioMixerPlayable.bindings.cs", { 61, 101, 161, 191, 246, 243, 230, 247, 173, 244, 46, 184, 65, 58, 4, 90} },
+};
+#else
+static const Il2CppSequencePointSourceFile g_sequencePointSourceFiles[1] = { NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppTypeSourceFilePair g_typeSourceFiles[6] = 
+{
+	{ 5500, 1 },
+	{ 5499, 1 },
+	{ 5503, 1 },
+	{ 5507, 2 },
+	{ 5508, 3 },
+	{ 5509, 4 },
+};
+#else
+static const Il2CppTypeSourceFilePair g_typeSourceFiles[1] = { { 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodScope g_methodScopes[12] = 
+{
+	{ 0, 26 },
+	{ 0, 11 },
+	{ 0, 84 },
+	{ 0, 11 },
+	{ 0, 28 },
+	{ 0, 28 },
+	{ 0, 29 },
+	{ 0, 29 },
+	{ 0, 12 },
+	{ 0, 24 },
+	{ 0, 12 },
+	{ 0, 24 },
+};
+#else
+static const Il2CppMethodScope g_methodScopes[1] = { { 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodHeaderInfo g_methodHeaderInfos[28] = 
+{
+	{ 26, 0, 1 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 11, 1, 1 },
+	{ 84, 2, 1 },
+	{ 11, 3, 1 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 28, 4, 1 },
+	{ 28, 5, 1 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 29, 6, 1 },
+	{ 29, 7, 1 },
+	{ 0, 0, 0 },
+	{ 0, 0, 0 },
+	{ 12, 8, 1 },
+	{ 24, 9, 1 },
+	{ 12, 10, 1 },
+	{ 24, 11, 1 },
+};
+#else
+static const Il2CppMethodHeaderInfo g_methodHeaderInfos[1] = { { 0, 0, 0 } };
+#endif
+IL2CPP_EXTERN_C const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationUnityEngine_AudioModule;
+const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationUnityEngine_AudioModule = 
+{
+	(Il2CppMethodExecutionContextInfo*)g_methodExecutionContextInfos,
+	(Il2CppMethodExecutionContextInfoIndex*)g_methodExecutionContextInfoIndexes,
+	(Il2CppMethodScope*)g_methodScopes,
+	(Il2CppMethodHeaderInfo*)g_methodHeaderInfos,
+	(Il2CppSequencePointSourceFile*)g_sequencePointSourceFiles,
+	131,
+	(Il2CppSequencePoint*)g_sequencePointsUnityEngine_AudioModule,
+	0,
+	(Il2CppCatchPoint*)g_catchPoints,
+	6,
+	(Il2CppTypeSourceFilePair*)g_typeSourceFiles,
+	(const char**)g_methodExecutionContextInfoStrings,
+};
